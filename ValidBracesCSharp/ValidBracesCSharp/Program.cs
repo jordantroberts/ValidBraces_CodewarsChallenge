e8998cd210b47b94359535b3061223a522b67252
Jordan Roberts <jordanroberts@talk21.com>
2019-09-26 11:48:37 +0100

    Added user input so they can type a string to console

--- ValidBracesCSharp/ValidBracesCSharp/Program.cs
+++ ValidBracesCSharp/ValidBracesCSharp/Program.cs
@@ -1,45 +1,35 @@
 ﻿using System;
-using System.Linq;
 using System.Collections;
+using System.Linq;
 
-namespace ValidBracesCSharp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace ValidBracesCSharp {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine("Please enter a string of braces");
+            string input = Console.ReadLine();
+            Console.WriteLine(Brace.validBraces(input));
         }
     }
 }
 
+public class Brace {
+    public static bool validBraces (String braces) {
+        Stack checkedChars = new Stack ();
 
-public class Brace
-{
-    public static bool validBraces(String braces)
-    {
-        Stack checkedChars = new Stack();
-
-        for (int i = 0; i < braces.Length; i++)
-        {
-            if (braces[i] == '(' || braces[i] == '{' || braces[i] == '[')
-            {
-                checkedChars.Push(braces[i]);
-            } else if (checkedChars.Count == 0)
-            {
+        for (int i = 0; i < braces.Length; i++) {
+            if (braces[i] == '(' || braces[i] == '{' || braces[i] == '[') {
+                checkedChars.Push (braces[i]);
+            } else if (checkedChars.Count == 0) {
                 return false;
-            } else if ((braces[i].ToString() == ")" && checkedChars.Peek().ToString() == "(") || (braces[i].ToString() == "}" && checkedChars.Peek().ToString() == "{") || (braces[i].ToString()== "]" && checkedChars.Peek().ToString() == "["))
-            {
-                checkedChars.Pop();
-            } else
-            {
+            } else if ((braces[i].ToString () == ")" && checkedChars.Peek ().ToString () == "(") || (braces[i].ToString () == "}" && checkedChars.Peek ().ToString () == "{") || (braces[i].ToString () == "]" && checkedChars.Peek ().ToString () == "[")) {
+                checkedChars.Pop ();
+            } else {
                 break;
             }
         }
 
-        if (checkedChars.Count != 0)
-        {
-            return false; 
+        if (checkedChars.Count != 0) {
+            return false;
         }
 
         return true;
