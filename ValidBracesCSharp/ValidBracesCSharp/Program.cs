a4d947eb62be52fb30b34b5116a9ead6d3e00740
Jordan Roberts <jordanroberts@talk21.com>
2019-09-26 11:26:34 +0100

    Set up project and added tests from Codewars

---
+++ ValidBracesCSharp/ValidBracesCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ValidBracesCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
