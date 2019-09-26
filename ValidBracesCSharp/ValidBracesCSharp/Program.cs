15834d33f2c56dcf3b1fb43f1f39327884b8368f
Jordan Roberts <jordanroberts@talk21.com>
2019-09-26 19:40:28 +0100

    Neatened up so line length wasn't really long

--- ValidBracesCSharp/ValidBracesCSharp/Program.cs
+++ ValidBracesCSharp/ValidBracesCSharp/Program.cs
@@ -26,11 +26,16 @@ public class Brace {
         Stack checkedChars = new Stack ();
 
         for (int i = 0; i < braces.Length; i++) {
+
+            var matchCurveBrace = braces[i].ToString() == ")" && checkedChars.Peek().ToString() == "(";
+            var matchStraightBrace = braces[i].ToString() == "]" && checkedChars.Peek().ToString() == "[";
+            var matchCurlyBrace = braces[i].ToString() == "}" && checkedChars.Peek().ToString() == "{";
+
             if (braces[i] == '(' || braces[i] == '{' || braces[i] == '[') {
                 checkedChars.Push (braces[i]);
             } else if (checkedChars.Count == 0) {
                 return false;
-            } else if ((braces[i].ToString () == ")" && checkedChars.Peek ().ToString () == "(") || (braces[i].ToString () == "}" && checkedChars.Peek ().ToString () == "{") || (braces[i].ToString () == "]" && checkedChars.Peek ().ToString () == "[")) {
+            } else if (matchCurveBrace || matchCurlyBrace || matchStraightBrace) {
                 checkedChars.Pop ();
             } else {
                 break;
